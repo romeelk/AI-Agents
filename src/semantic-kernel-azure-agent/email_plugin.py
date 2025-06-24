d13c10a04984adcfdb4fe76c30b53cb7e74173c1
romeelk <romeel_k@yahoo.co.uk>
2025-06-24 16:37:04 +0100

    Completed expense agent demo

---
+++ src/semantic-kernel-azure-agent/email_plugin.py
@@ -0,0 +1,21 @@
+from semantic_kernel.functions import kernel_function
+
+class EmailPlugin:
+    
+    @kernel_function(
+        description="Send an expense claim email to the expenses team.",
+    )
+    def send_expense_email(self, to:str, user:str, subject:str, body:str):
+        body = f"""
+        To: {to}
+        Subject: {subject}
+
+        Dear Expenses Team,
+
+        Please find attached the itemised expense claim for {user}.\n{body}
+
+        Regards,
+        {user}
+        """
+        print(body)
+        return body
