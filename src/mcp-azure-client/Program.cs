154eeb6b5bd0efb43dda6a4973b0efae07d7afa6
romeelk <romeel_k@yahoo.co.uk>
2025-09-09 12:31:36 +0100

    Dotnet mcp azure client demo

--- src/mcp-azure-client/Program.cs
+++ src/mcp-azure-client/Program.cs
@@ -2,14 +2,33 @@
 using Azure.Identity;
 using Microsoft.Extensions.AI;
 using ModelContextProtocol.Client;
-using ModelContextProtocol.Protocol;
+
+using Microsoft.Extensions.Configuration;
+
+IConfiguration configuration = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+    .AddEnvironmentVariables()
+    .AddCommandLine(args)
+    .Build();
+
+static (string? azoaEndpoint, string? azoaApiKey, string? aoaiModelId, string? azoaDeployedModel) GetConfig(IConfiguration config)
+{
+    var azoaEndpoint = config["azoaEndpoint"];
+    var azoaApikey = config["azoaApiKey"];
+    var azoaModelId = config["azoaModelId"];
+    var azoaDeployedModel = config["azoaDeployedModel"];
+    return (azoaEndpoint, azoaApikey, azoaModelId, azoaDeployedModel);
+}
+
 Console.WriteLine("C# MCP client to talk to Azure MCP server ");
+var (azoaEndpoint, azoaApikey, azoaModel, azoaDeployedModel) = GetConfig(configuration);
 
 IChatClient client =
     new ChatClientBuilder(
-        new AzureOpenAIClient(new Uri("https://sk-agent-demo-rk.cognitiveservices.azure.com/>"),
+        new AzureOpenAIClient(new Uri(azoaEndpoint),
         new DefaultAzureCredential())
-        .GetChatClient("gpt-40").AsIChatClient())
+        .GetChatClient("gpt-4o").AsIChatClient())
         .UseFunctionInvocation()
         .Build();
 
@@ -27,8 +46,34 @@ var mcpClient = await McpClientFactory.CreateAsync(
 
 var availableTools = await mcpClient.ListToolsAsync();
 
+Console.WriteLine($"There are a total of {availableTools.Count} Azure tools");
 foreach (var tool in availableTools)
 {
-    Console.WriteLine($"Tool name: {tool.Name}, Deccription: {tool.Description}");
+    Console.WriteLine($"Tool name: {tool.Name}");
 }
 
+List<ChatMessage> messages = [];
+
+while (true)
+{
+    Console.WriteLine("Prompt(e to exit):");
+
+    var input = Console.ReadLine();
+
+    if (input == "e") break;
+
+    messages.Add(new(ChatRole.User, input));
+
+    // Get responses from LLM
+    List<ChatResponseUpdate> llmResponses = [];
+    await foreach (var response in client.GetStreamingResponseAsync(messages, new() { Tools = [.. availableTools] }))
+    {
+        // print response
+        Console.Write(response);
+        // add to list of chat messages
+        llmResponses.Add(response);
+    }
+    Console.WriteLine();
+    messages.AddMessages(llmResponses);
+}
+Console.WriteLine("Thankyou for using Azure MCP server chat!");
--- src/mcp-azure-client/readme.md
+++ src/mcp-azure-client/readme.md
@@ -3,3 +3,19 @@
 This demo shows how to connect to a MCP server to query about Azure information.
 
 It uses Azure AI Foundry to create an AI agent that can use the mcp tool.
+
+## Pre-requesites
+
+1) latest Node version. Make sure you have a node version > v20
+- on Mac run  nvm install node —lts
+2) Access to azure subscription
+3) VS Code installed
+
+
+## Verify azure mcp server
+
+You can verify the node package @azure/mcp@latest runs 
+
+```
+ npx -y @azure/mcp@latest server start
+```
